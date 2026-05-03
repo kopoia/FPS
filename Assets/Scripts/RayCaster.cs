3e9a654c3b024d064984363f77487e652eeef9e2
kopoia <kopoia0707@gmail.com>
2026-05-03 17:42:53 +0900

    first commit

---
+++ Assets/Scripts/RayCaster.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    public Camera playerCamera;
+    public int enemykillcount;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Cursor.lockState = CursorLockMode.Locked; // カーソルをロックする 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) // 左クリックが押されたとき
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        int distance = 50;
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0); // 画面の中心を取得
+        Ray ray = playerCamera.ScreenPointToRay(center); // 画面の中心からRayを飛ばす
+        RaycastHit hitInfo; // Rayが当たったオブジェクトの情報を格納する変数
+        if(Physics.Raycast(ray, out hitInfo, distance)) // Rayがオブジェクトに当たったとき
+        {
+            if(hitInfo.collider.gameObject.tag == "Enemy") // Rayが当たったオブジェクトのタグが"Enemy"のとき
+            {
+            }
+        }
+    }
+}
